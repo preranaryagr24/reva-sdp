0969acf896940c356b50eaf333e16f01b61d488c
Preran Arya  G R <prerannaryagr@gmail.com>
2021-12-20 12:08:57 +0530

    change 2

--- reva-sdp/src/main/java/com/cynefian/revasdp/SkillsDevelopmentProgram.java
+++ reva-sdp/src/main/java/com/cynefian/revasdp/SkillsDevelopmentProgram.java
@@ -8,6 +8,7 @@ public class SkillsDevelopmentProgram {
 	
 	public static void main(String[] args) {
 		System.out.println)("Preran arya g r");
+		System.out.print("Arya")
 		System.out.println("Welcome to the Skills Development Program");
 		logMessage();
 	s}
